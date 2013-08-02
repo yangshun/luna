1ef76615c995ab88601ab151d7137f68e8472829
Yang Shun <tay.yang.shun@gmail.com>
2013-08-02 20:07:32 +0800

    added parent controller

--- js/luna.js
+++ js/luna.js
@@ -9,10 +9,21 @@ angular.module('luna', []).
     otherwise({redirectTo: '/'});
 }]);
 
-function LandingCtrl($scope, $http) {
+function LunaBlogCtrl($scope, $http) {
   $http.get('content/posts.json').success(function(data) {
     $scope.posts = data;
+    // for (var i = 0; i < $scope.posts.length; i++) {
+    //   fetchFullPostContent(i);
+    // }
+    // function fetchFullPostContent(index) {
+    //   $http.get('posts/' + $scope.posts[index].route + '.md').success(function(data) {
+    //     $scope.posts[index].content = converter.makeHtml(data);
+    //   });
+    // }
   });
+}
+
+function LandingCtrl($scope, $http) {
 
   $scope.orderProp = 'timestamp';
 }
@@ -26,8 +37,18 @@ function AllPostsCtrl($scope, $http) {
 }
 
 function SinglePostCtrl($scope, $routeParams, $http) {
-  console.log($routeParams.post_id);
-  $http.get('posts/' + $routeParams.post_id + '.md').success(function(data) {
+
+  function findFileNameFromPostId(post_id) {
+    console.log($scope.$parent.posts);
+    for (var i = 0; i < $scope.$parent.posts.length; i++) {
+      if ($scope.$parent.posts[i].post_id === post_id) {
+        var filename = $scope.$parent.posts[i].filename;
+        return filename;
+      }
+    }
+    return ''; // TODO: Redirect to a 404 page
+  }
+  $http.get('posts/' + findFileNameFromPostId($routeParams.post_id)).success(function(data) {
     console.log(data);
     $scope.content = converter.makeHtml(data);
   });
--- run.py
+++ run.py
@@ -11,7 +11,7 @@ for file_path in file_paths:
     current_file_path = POSTS_DIR_PATH + file_path
     with open(current_file_path) as f:
         post_title = f.readline()
-        throw_away = f.readline()
+        throw_away = f.readline()   # second line of post is markdown heading syntax
         file_content = f.read()
 
     post = {
@@ -19,12 +19,13 @@ for file_path in file_paths:
             'modified': int(os.path.getmtime(current_file_path)),
             'content': file_content.strip(),
             'title': post_title.replace('\n', ''),
-            'route': file_path[:-3]
+            'filename': file_path,
+            'post_id': post_title.replace(' ', '-').lower().strip() # TODO: Make sure there are no duplicate ids
             }
     json_output.append(post)
 
 sorted(json_output, key=lambda post: post['timestamp'])
-# print json.dumps(json_output, None, indent=4)
+print json.dumps(json_output, None, indent=4)
 
 posts_json_file = open(POSTS_JSON_FILE_PATH, 'w+')
 # print posts_json_file
