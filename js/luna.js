ad5277af2bfdb0de3922fd1aee9bab231a9942e2
Yang Shun <tay.yang.shun@gmail.com>
2013-08-03 17:50:48 +0800

    Revert "wrap posts loading in callback"
    This reverts commit 5d125148168f1dfc6141c1e2bf0015ff479af3b1.

--- js/luna.js
+++ js/luna.js
@@ -12,48 +12,32 @@ angular.module('luna', []).
 
 function LunaBlogCtrl($scope, $http) {
 
-  $scope.fetchPosts = function(cb) {
-    if (!$scope.posts) {
-      $http.get('content/posts.json').success(function(data) {
-        $scope.posts = data;
-        for (var i = 0; i < $scope.posts.length; i++) {
-          $scope.posts[i].content = converter.makeHtml($scope.posts[i].content);
-          var d = new Date($scope.posts[i].timestamp * 1000);
-          $scope.posts[i].date = d.toString();
-        }
-        console.log('loading done');
-        applyCb(cb);
-      });
-    } else {
-      applyCb(cb);
+  $http.get('content/posts.json').success(function(data) {
+    $scope.posts = data;
+    for (var i = 0; i < $scope.posts.length; i++) {
+      $scope.posts[i].content = converter.makeHtml($scope.posts[i].content);
+      var d = new Date($scope.posts[i].timestamp * 1000);
+      $scope.posts[i].date = d.toString();
     }
-    function applyCb(cb) {
-      if (cb) { cb(); } else { return; }
-    }
-  };
+    console.log('loading done');
+  });
 }
 
 function LandingCtrl($scope, $routeParams) {
   console.log('landing ctrl init');
-  $scope.$parent.fetchPosts(getPagePosts);
-  
-  function getPagePosts() {
-    if ($scope.posts) {
-      $scope.current_page = $routeParams.page ? parseInt($routeParams.page) : 1;
-      $scope.prev_page = $scope.current_page - 1 > 0 ? $scope.current_page - 1 : undefined;
-      $scope.next_page = ($scope.current_page * CONFIG.NUM_POSTS_PER_PAGE) < $scope.posts.length ? $scope.current_page + 1 : undefined;
-      var starting_index = Math.max($scope.current_page - 1, 0) * CONFIG.NUM_POSTS_PER_PAGE;
-      $scope.current_page_posts = $scope.posts.slice(starting_index, starting_index + CONFIG.NUM_POSTS_PER_PAGE);
-      $scope.orderProp = 'timestamp';
-      console.log('current page posts init');
-    }
+  if ($scope.posts) {
+    $scope.current_page = $routeParams.page ? parseInt($routeParams.page) : 1;
+    $scope.prev_page = $scope.current_page - 1 > 0 ? $scope.current_page - 1 : undefined;
+    $scope.next_page = ($scope.current_page * CONFIG.NUM_POSTS_PER_PAGE) < $scope.posts.length ? $scope.current_page + 1 : undefined;
+    var starting_index = Math.max($scope.current_page - 1, 0) * CONFIG.NUM_POSTS_PER_PAGE;
+    $scope.current_page_posts = $scope.posts.slice(starting_index, starting_index + CONFIG.NUM_POSTS_PER_PAGE);
+    $scope.orderProp = 'timestamp';
+    console.log('current page posts init');
   }
 }
 
 function AllPostsCtrl($scope) {
-  $scope.$parent.fetchPosts(function() {
-    $scope.orderProp = 'timestamp';
-  });
+  $scope.orderProp = 'timestamp';
 }
 
 function SinglePostCtrl($scope, $routeParams) {
@@ -61,9 +45,7 @@ function SinglePostCtrl($scope, $routeParams) {
   $scope.post_loaded = false;
   $scope.newer_post_id = undefined;
   $scope.older_post_id = undefined;
-  $scope.$parent.fetchPosts(function() {
-    $scope.current_post = findPostFromPostId($routeParams.post_id);
-  });
+  $scope.current_post = findPostFromPostId($routeParams.post_id);
 
   function findPostFromPostId(post_id) {
     for (var i = 0; i < $scope.$parent.posts.length; i++) {
