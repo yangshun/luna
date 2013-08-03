c6eb2967656ef4cfd77eea933b66e2eaa54de2c8
Yang Shun <tay.yang.shun@gmail.com>
2013-08-03 18:11:54 +0800

    use $broadcast and $on instead

--- js/luna.js
+++ js/luna.js
@@ -10,7 +10,9 @@ angular.module('luna', []).
     otherwise({redirectTo: '/'});
 }]);
 
-function LunaBlogCtrl($scope, $http) {
+function LunaCtrl($scope, $http, $timeout) {
+  console.log('luna ctrl init');
+  $scope.all_posts_loaded = false;
 
   $http.get('content/posts.json').success(function(data) {
     $scope.posts = data;
@@ -19,13 +21,25 @@ function LunaBlogCtrl($scope, $http) {
       var d = new Date($scope.posts[i].timestamp * 1000);
       $scope.posts[i].date = d.toString();
     }
-    console.log('loading done');
+    console.log('all posts loaded');
+    $scope.all_posts_loaded = true;
+    $timeout(function() {
+      $scope.$broadcast('allPostsLoaded');
+    }, 0);
   });
 }
 
 function LandingCtrl($scope, $routeParams) {
   console.log('landing ctrl init');
-  if ($scope.posts) {
+
+  console.log('all posts loaded: ' + $scope.$parent.all_posts_loaded);
+  if ($scope.$parent.all_posts_loaded) {
+    getPagePosts();
+  } else {
+    $scope.$on('allPostsLoaded', getPagePosts);
+  }
+  
+  function getPagePosts() {
     $scope.current_page = $routeParams.page ? parseInt($routeParams.page) : 1;
     $scope.prev_page = $scope.current_page - 1 > 0 ? $scope.current_page - 1 : undefined;
     $scope.next_page = ($scope.current_page * CONFIG.NUM_POSTS_PER_PAGE) < $scope.posts.length ? $scope.current_page + 1 : undefined;
@@ -34,6 +48,7 @@ function LandingCtrl($scope, $routeParams) {
     $scope.orderProp = 'timestamp';
     console.log('current page posts init');
   }
+
 }
 
 function AllPostsCtrl($scope) {
@@ -45,19 +60,26 @@ function SinglePostCtrl($scope, $routeParams) {
   $scope.post_loaded = false;
   $scope.newer_post_id = undefined;
   $scope.older_post_id = undefined;
-  $scope.current_post = findPostFromPostId($routeParams.post_id);
 
-  function findPostFromPostId(post_id) {
-    for (var i = 0; i < $scope.$parent.posts.length; i++) {
-      if ($scope.$parent.posts[i].post_id === post_id) {
-        console.log(i);
-        $scope.post_loaded = true;
-        $scope.newer_post_id = i - 1 >= 0 ? $scope.$parent.posts[i-1].post_id : undefined;
-        $scope.older_post_id = i + 1 <= $scope.$parent.posts.length - 1 ? $scope.$parent.posts[i+1].post_id : undefined;
-
-        return $scope.$parent.posts[i];
-      }
-    }
-    return ''; // TODO: Redirect to a 404 page
+  if ($scope.$parent.all_posts_loaded) {
+    loadSinglePost();
+  } else {
+    $scope.$on('allPostsLoaded', loadSinglePost);
   }
+
+  function loadSinglePost() {
+    $scope.current_post = findPostFromPostId($routeParams.post_id);
+    function findPostFromPostId(post_id) {
+      for (var i = 0; i < $scope.$parent.posts.length; i++) {
+        if ($scope.$parent.posts[i].post_id === post_id) {          
+          $scope.post_loaded = true;
+          $scope.newer_post_id = i - 1 >= 0 ? $scope.$parent.posts[i-1].post_id : undefined;
+          $scope.older_post_id = i + 1 <= $scope.$parent.posts.length - 1 ? $scope.$parent.posts[i+1].post_id : undefined;
+          return $scope.$parent.posts[i];
+        }
+      }
+      return ''; // TODO: Redirect to a 404 page
+    }
+  }
+
 }
